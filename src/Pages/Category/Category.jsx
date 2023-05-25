318533c4932e561c50d56d83a84c572e3f3dbc84
Rihanul Islam <rihanthedev@gmail.com>
2023-05-26 04:10:11 +0600

    SectionTitle

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -10,12 +10,17 @@ import slide2 from '../../assets/home/slide2.jpg'
 import slide3 from '../../assets/home/slide3.jpg'
 import slide4 from '../../assets/home/slide4.jpg'
 import slide5 from '../../assets/home/slide5.jpg'
+import SectionTitle from '../../Components/SectionTitle/SectionTitle';
 
 const Category = () => {
     return (
         <div className='my-2'>
+
+            <SectionTitle
+            heading={'From Online'}
+            subHeading={'From 11:00 TO 6:30'}
+            ></SectionTitle>
             <Swiper
-        
                 slidesPerView={4}
                 spaceBetween={30}
                 centeredSlides={true}
