f53053dea9e5da93d09303969b20e0a33b00e6c2
Rihanul Islam <rihanthedev@gmail.com>
2023-05-26 05:16:15 +0600

    Project day one

--- src/Pages/Featured/Featured.jsx
+++ src/Pages/Featured/Featured.jsx
@@ -4,7 +4,7 @@ import image1 from '../../assets/home/featured.jpg'
 import './Feature.css'
 const Featured = () => {
     return (
-        <div className="mb-2 my-20 py-10  feature-item">
+        <div className="mb-2 bg-fixed bg-opacity-10 text-white my-20 py-10  feature-item">
             <SectionTitle
             heading={'Our Featured'}
             subHeading={'Check It Out'}
@@ -17,7 +17,7 @@ const Featured = () => {
                     <p>Aug 15,2090</p>
                     <p className="uppercase text-2xl">Where Can I get some?</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit fugiat blanditiis animi nam rerum ex beatae nostrum aliquam? Ea odit nostrum provident ab commodi quo aperiam praesentium! Aperiam reprehenderit porro odit labore fugiat, non laborum totam natus voluptatum, quo inventore, distinctio voluptatem expedita iure iusto. Explicabo nesciunt porro reprehenderit eos!</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-2">Order Now</button>
                 </div>
             </div>
         </div>
