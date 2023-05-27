125fe1630fe6df564af9cde49ac0d01b91592d62
Rihanul Islam <rihanthedev@gmail.com>
2023-05-27 20:29:22 +0600

    React Parallax

--- src/Pages/Menu/Menu/OurMenu.jsx
+++ src/Pages/Menu/Menu/OurMenu.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import Menu from '../../PopularMenu/Menu'
 const OurMenu = () => {
     return (
         <div>
             <Helmet>
-                <title>Hello</title>
+                <title>Bestro Boss || Menu</title>
             </Helmet>
-            <h2>Men men Menu</h2>
+            <Cover img={menuImg} title='our Menu'></Cover>
+            <Menu></Menu>
+            <Cover img={menuImg} title='our Menu'></Cover>
+            <Menu></Menu>
+            <Cover img={menuImg} title='our Menu'></Cover>
+            <Menu></Menu>
+            <Cover img={menuImg} title='our Menu'></Cover>
+            <Menu></Menu>
+            <Cover img={menuImg} title='our Menu'></Cover>
+            
         </div>
     );
 };
