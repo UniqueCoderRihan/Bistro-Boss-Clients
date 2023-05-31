46f2f2d0b6d78676b4e04e2f1c2eaca7539e1741
Rihanul Islam <rihanthedev@gmail.com>
2023-06-01 01:35:56 +0600

    Vaildation added

--- src/Pages/User/SingUp.jsx
+++ src/Pages/User/SingUp.jsx
@@ -10,7 +10,6 @@ const SingUp = () => {
     const onSubmit = data => {
         console.log(data)
     };
-    console.log(watch("example"));
 
     return (
         <div className="hero min-h-screen bg-pink-200 rounded-lg">
@@ -29,19 +28,24 @@ const SingUp = () => {
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" {...register("name")} name='name' required placeholder="Name" className="input input-bordered" />
+                                <input type="text" {...register("name",{ required: true })} name='name' placeholder="Name" className="input input-bordered" />
+                                {errors.name && <span className='text-red-600'>Name is required</span>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" {...register("email")} name='email' required placeholder="email" className="input input-bordered" />
+                                <input type="email" {...register("email",{required:true})} name='email' placeholder="email" className="input input-bordered" />
+                                {errors.email && <span className='text-red-600'>Email is required</span>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input {...register("password")} type="password" name='password' required placeholder="password" className="input input-bordered" />
+                                <input {...register("password",{ pattern: / ^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z].*[a-z])$ / , required: true, minLength: 6, maxLength: 20 })} type="password" name='password' placeholder="password" className="input input-bordered" />
+                                {errors.password?.type === 'minLength' && <span className='text-red-600'>Password minimum 6 character is required</span>}
+                                {errors.password?.type === 'required' && <span className='text-red-600'>Password Is Required</span>}
+                                {errors.password?.type === 'pattern' && <span className='text-red-600'>Must have 1 Uppercase & 2 LowerCase</span>}
                                 <label className="label">
                                     <div className="flex">
                                         <p className="label-text-alt link link-hover">Alredy have an Account? <span><Link to='/login'>Login</Link></span> </p>
