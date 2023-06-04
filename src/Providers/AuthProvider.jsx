7ba228b9b1cdb2e4d4203700b967a9223c29b00a
Rihanul Islam <rihanthedev@gmail.com>
2023-06-05 01:59:47 +0600

    done the client side token also Local storge

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import {GoogleAuthProvider , createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from '../Firebase/firebase.config'
+import axios from "axios";
 
 /*
  *TODO: Update new Spinner 
@@ -43,6 +44,17 @@ useEffect(() => {
     const unsubcrive = onAuthStateChanged(auth, currentUser => {
         setUser(currentUser)
         console.log("currentUser", currentUser);
+        if(currentUser){
+            axios.post('http://localhost:5000/jwt',{email:currentUser.email})
+            .then(data=>{
+                console.log(data.data);
+                localStorage.setItem('access-token',data.data)
+            })
+        }
+        else{
+            console.log('user to null tai ');
+            localStorage.removeItem('access-token')
+        }
         setLoading(false)
     })
     return () => {
