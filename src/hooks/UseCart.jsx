e09a503b811bff05a2a20c3f574ecd19a65a7a56
Rihanul Islam <rihanthedev@gmail.com>
2023-06-05 02:40:37 +0600

    Authorization sent

--- src/hooks/UseCart.jsx
+++ src/hooks/UseCart.jsx
@@ -5,11 +5,14 @@ import { useQuery } from "@tanstack/react-query";
 
 const UseCart = ()=>{
     const {user} = useContext(AuthContex);
-
+    const token = localStorage.getItem('access-token');
+// you need to add token on Fetch Headers.
     const {refetch,data: cart=[]} = useQuery({
         queryKey: ['cart',user?.email],
         queryFn: async ()=>{
-            const res = await fetch(`http://localhost:5000/carts?email=${user.email}`)
+            const res = await fetch(`http://localhost:5000/carts?email=${user.email}`,{headers:{
+                authorization: `bearer ${token}`
+            }})
             return res.json();
         }
         
