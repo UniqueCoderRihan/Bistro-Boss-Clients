4e4cc0464dcbc6587924310646e5dc2b71f21ae4
Rihanul Islam <rihanthedev@gmail.com>
2023-06-06 16:04:08 +0600

    Card Vaildation Checked

--- src/Pages/Dashbord/Payment/Checkout.jsx
+++ src/Pages/Dashbord/Payment/Checkout.jsx
@@ -1,43 +1,59 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const Checkout = () => {
     const stripe = useStripe();
     const elements = useElements();
-
-    const handleSubmit=(event)=>{
+    const [cardError, setError] = useState();
+    const handleSubmit = async (event) => {
         event.preventDefault();
 
-        if(!stripe || !elements){
+        if (!stripe || !elements) {
             return;
         }
         const card = elements.getElement(CardElement);
-        if(card == null){
+        if (card == null) {
             return;
         }
-
+        console.log(card);
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+        if (error) {
+            console.log('error', error);
+            setError(error.message)
+        }
+        else {
+            setError('')
+            console.log('Payment Method: ', paymentMethod);
+        }
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form className="w-2/3 m-8" onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <button className="btn btn-primary btn-sm mt-4" type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+            </form>
+            {cardError && <p className="text-red-600 ml-8">{cardError}</p>}
+        </>
     );
 };
 
--- src/Pages/Dashbord/Payment/Payment.jsx
+++ src/Pages/Dashbord/Payment/Payment.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+
+import { Elements } from '@stripe/react-stripe-js';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle'
+import Checkout from './Checkout';
+import { loadStripe } from '@stripe/stripe-js';
+
+// TODO:Publish Key
+const stripePromis = loadStripe(import.meta.env.VITE_payment_Stripe_Key)
 const Payment = () => {
     return (
         <div>
             <SectionTitle subHeading='Please Proceed to Checkout' heading='Payment'></SectionTitle>
-            
+            <Elements stripe={stripePromis}>
+                <Checkout></Checkout>
+            </Elements>
         </div>
     );
 };
